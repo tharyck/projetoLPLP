41c517b3d323ca2ab24d4606cf47786ce76166ed
jefesonf <jefersonnpn@gmail.com>
2017-06-02 21:23:47 -0300

    start up

---
+++ hunt-the-wumpus/game.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAP_SIZE 100
+
+enum world {ME, GOLD, PIT, STENCH, BREEZE, WUMPUS};
+
+int Map[MAP_SIZE][MAP_SIZE];
+
+int main(int argc, char *argv[])
+{
+
+  // read imput argv => size grid and player name
+
+  return 0;
+}
